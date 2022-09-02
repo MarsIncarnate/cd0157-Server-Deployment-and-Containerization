11d270281ee696571364d37e154a9baea49fb014
Mars-Reborn <106602563+Mars-Reborn@users.noreply.github.com>
2022-09-02 03:03:44 +0100

    c

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from flask import Flask, jsonify, request, abort
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
+# 
 
 def _logger():
     
