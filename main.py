1ff2192c568475bbe6afba02276f54b8e7f09ebf
Mars-Reborn <106602563+Mars-Reborn@users.noreply.github.com>
2022-09-02 02:02:35 +0100

    checking codebuild

--- main.py
+++ main.py
@@ -17,11 +17,7 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
 def _logger():
-    '''
-    Setup logger format, level, and handler.
-
-    RETURNS: log object
-    '''
+    
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     log = logging.getLogger(__name__)
